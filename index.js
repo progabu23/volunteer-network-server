809dd57cf58a92c4a0b98dc2253043bc1293250b
progabu23 <devabu23>
2020-10-06 21:35:29 +0600

    code update

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
+app.get('/' , (req, res) =>{
+    res.send('Hello From MongoDB ITS Working')
+})
+
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config()
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xc4gw.mongodb.net/volunteerNetwork?retryWrites=true&w=majority`;
